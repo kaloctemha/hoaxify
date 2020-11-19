bcea8200eb08d8ae5822402d4c193951bddcac5b
Ahmet COLAK <acolak@netas.com.tr>
2020-11-19 13:18:57 +0300

    202011191318

--- course-tr/ws/src/main/java/com/hoaxify/ws/Configuration/SecurityConfiguration.java
+++ course-tr/ws/src/main/java/com/hoaxify/ws/Configuration/SecurityConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
